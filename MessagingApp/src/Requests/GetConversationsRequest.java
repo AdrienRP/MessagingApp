5bd808cb2c8f5c014ddc60aa7fdef869d2bad23a
Cameron Killins <cameronkillins@gmail.com>
2023-03-29 22:33:58 -0400

    added Inbox List support(probably not working fully)
    Note that there is no support for viewing the conversation when clicking
    on it in the inbox yet.

---
+++ MessagingApp/src/Requests/GetConversationsRequest.java
@@ -0,0 +1,11 @@
+package Requests;
+
+public class GetConversationsRequest extends Request{
+	private static final long serialVersionUID = 1L;
+	
+	public GetConversationsRequest() {
+		super("GetConversationsRequest");
+	}
+	
+
+}
